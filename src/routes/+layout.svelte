ecb2f3ad4d5d50f0f4f07102c161032107f29a60
Timo Bickmann (MB Air) <bickmann.timo@gmail.com>
2023-11-08 19:11:53 +0100

    added icons

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,22 +1,21 @@
 <script lang="ts">
 	import '../app.postcss';
 	import { AppShell, AppBar } from '@skeletonlabs/skeleton';
+	import GithubIcon from '~icons/fa/github';
 </script>
-
 
 <AppShell>
 	<svelte:fragment slot="header">
-		
 		<AppBar gridColumns="grid-cols-3" slotDefault="place-self-center" slotTrail="place-content-end">
 			<svelte:fragment slot="lead">
-				<strong class="text-xl uppercase">Logo</strong>
+				<img class="h-8" src="favicon.png" alt="app logo" />
 			</svelte:fragment>
-			<strong class="text-lg">Timos Learning Hub</strong> 
+			<strong class="text-xl">Timos Learning Hub</strong>
 			<svelte:fragment slot="trail">
-				<a href="#">github repo</a>
+				<a href="https://github.com/timobickmann/webdev-learning-hub"><GithubIcon /> </a>
 			</svelte:fragment>
 		</AppBar>
 	</svelte:fragment>
-	
+
 	<slot />
 </AppShell>
