2f2eac7614115fd089c52dcf4dc28e2f9e784272
Timo Bickmann (MB Air) <bickmann.timo@gmail.com>
2023-11-08 18:14:53 +0100

    initial commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+	import '../app.postcss';
+	import { AppShell, AppBar } from '@skeletonlabs/skeleton';
+</script>
+
+
+<AppShell>
+	<svelte:fragment slot="header">
+		
+		<AppBar gridColumns="grid-cols-3" slotDefault="place-self-center" slotTrail="place-content-end">
+			<svelte:fragment slot="lead">
+				<strong class="text-xl uppercase">Logo</strong>
+			</svelte:fragment>
+			<strong class="text-lg">Timos Learning Hub</strong> 
+			<svelte:fragment slot="trail">
+				<a href="#">github repo</a>
+			</svelte:fragment>
+		</AppBar>
+	</svelte:fragment>
+	
+	<slot />
+</AppShell>
