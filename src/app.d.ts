fc2d3a023886764e49dd00962086059d4bf70c47
Timo Bickmann (MB Air) <bickmann.timo@gmail.com>
2023-11-12 17:35:21 +0100

    added auth handle hook

--- src/app.d.ts
+++ src/app.d.ts
@@ -1,15 +1,18 @@
 // See https://kit.svelte.dev/docs/types#app
 // for information about these interfaces
 // and what to do when importing types
-declare namespace App {
-	// interface Locals {}
+declare global {
+	
+	namespace App {
+	interface Locals {
+		auth: import('lucia').AuthRequest;
+	}
 	// interface PageData {}
 	// interface Error {}
 	// interface Platform {}
 }
 
 /// <reference types="lucia" />
-declare global {
 	namespace Lucia {
 		type Auth = import('$lib/server/lucia').Auth;
 		type DatabaseUserAttributes = object;
