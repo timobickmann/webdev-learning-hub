d23db19b88c563aa668207e5f1a4173ba3c504b2
Timo Bickmann (MB Air) <bickmann.timo@gmail.com>
2023-11-13 17:03:26 +0100

    removed userId from profile page

--- src/routes/profile/+page.server.ts
+++ src/routes/profile/+page.server.ts
@@ -6,7 +6,6 @@ export const load: PageServerLoad = async ({ locals }) => {
 	const session = await locals.auth.validate();
 	if (!session) throw redirect(302, '/login');
 	return {
-		userId: session.user.userId,
 		githubUsername: session.user.githubUsername
 	};
 };
--- src/routes/profile/+page.svelte
+++ src/routes/profile/+page.svelte
@@ -7,7 +7,6 @@
 </script>
 
 <h1>Profile</h1>
-<p>User id: {data.userId}</p>
 <p>GitHub username: {data.githubUsername}</p>
 <form method="POST" action="?/logout" use:enhance>
 	<input class="mt-5 btn cursor-pointer variant-filled" type="submit" value="Sign out" />
