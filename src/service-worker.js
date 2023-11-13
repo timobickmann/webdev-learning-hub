cf110ca79a25742a2172ef31e8e8ae68b43bc772
Timo Bickmann (MB Air) <bickmann.timo@gmail.com>
2023-11-13 21:06:08 +0100

    added service worker

---
+++ src/service-worker.js
@@ -0,0 +1,35 @@
+/// <reference types="@sveltejs/kit" />
+import { build, files, version } from '$service-worker';
+
+const CACHE = `cache-${version}`;
+
+const ASSETS = [
+	...build, // the app itself
+	...files // everything in `static`
+];
+
+self.addEventListener('install', (event) => {
+	// Create a new cache and add all files to it
+	async function addFilesToCache() {
+		const cache = await caches.open(CACHE);
+		await cache.addAll(ASSETS);
+	}
+
+	event.waitUntil(addFilesToCache());
+});
+
+self.addEventListener('activate', (event) => {
+	// Remove previous cached data from disk
+	async function deleteOldCaches() {
+		for (const key of await caches.keys()) {
+			if (key !== CACHE) await caches.delete(key);
+		}
+	}
+
+	event.waitUntil(deleteOldCaches());
+});
+
+self.addEventListener('fetch', () => {
+	// You can add custom fetch event logic here if needed
+	// For now, it does nothing, meaning it won't cache for offline use
+});
